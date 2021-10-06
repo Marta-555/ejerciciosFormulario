7b0443a1190e84807274a98fb377e78ff733aefc
Marta García Ortega <mgarort-555@g.educaand.es>
2021-10-06 10:08:00 +0200

    Procesamos el formulario en una única página

--- formulario.php
+++ formulario.php
@@ -5,14 +5,30 @@
     <meta http-equiv="Content-Type" content="text/html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
 </head>
+<?php
+    if(isset($_POST['operacion'])){
+        $numero1 = $_POST['numero1'];
+        $numero2 = $_POST['numero2'];
+
+        //valida($numero1, $numero2)
+
+
+        if (valida($numero1, $numero2) == true){       
+            pinta($numero1, $numero2);
+            //var_dump($_POST);
+        } else {
+            print "Error de contenido";
+        }
+    }
+?>
+
 <body>
     <?php
     
     ?>
 
-    <form action="procesa.php" method="POST">
+    <form action="" method="POST">
         <h3>FORMULARIOS - Comunicación con servidor</h3>
         <p>
             Suma:<br>
@@ -27,34 +43,34 @@
     </form>
 
     <?php
-        /*if(isset($_GET['operacion'])){
-            $numero1 = $_GET['numero1'];
-            $numero2 = $_GET['numero2'];
-        }
-        function pinta($numero1, $numero2){   
-            switch($_GET['operacion']){
-                $resultado = "";
-                case "Sumar":
-                    $resultado = "La suma de los números es ". ($numero1+$numero2);
-                break;
-                case "Restar":
-                    $resultado = "La resta de los números es ". ($numero1-$numero2);
-                break;
-                case "Multiplicar":
-                    $resultado = "La multiplicación de los números es ". ($numero1*$numero2);
-                break;
-                case "Dividir":
-                    $resultado = "La división de los números es ". ($numero1/$numero2);
-                break;
+       function valida($numero1, $numero2){
+            if(is_numeric($numero1) && is_numeric($numero2)){
+                return true;
+            } else {
+                return false;
             }
-            print $resultado;
-        } else {
-            echo "ERROR en el contenido";
+       }
+
+       function pinta($numero1, $numero2){
+        $resultado = "";
+        switch($_POST['operacion']){
+            case "Sumar":                    
+                $resultado = "La suma de los números es ". ($numero1+$numero2);
+            break;
+            case "Restar":
+                $resultado = "La resta de los números es ". ($numero1-$numero2);
+            break;
+            case "Multiplicar":
+                $resultado = "La multiplicación de los números es ". ($numero1*$numero2);
+            break;
+            case "Dividir":                    
+                $resultado = "La división de los números es ". ($numero1/$numero2);
+            break;
         }
-        */
-    
+        print $resultado;
+    }
 
-
+    //Valida tiene que devolver un array bidimiensional, asociativo(clave=al nombre que tenga post, valor), en el que se guardan los errores
     ?>
 
    
